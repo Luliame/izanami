47838d7a32e649d69b045e50d9ac9d7355df189a
Luliame <guillaume.valentin@etu.uca.fr>
2022-06-01 19:30:43 +0200

    using variable

--- Izanami/src/app/home/home.page.ts
+++ Izanami/src/app/home/home.page.ts
@@ -7,6 +7,30 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public temperature;
+  public humidity;
+  public commentary;
+  public location;
+  public visualisation;
+
+  public visuList = [
+    "sunny",
+    "partly-sunny",
+    "cloudy",
+    "rainy",
+    "thunderstorm",
+  
+    "cloudy-night",
+    "moon"
+  ]
+
+  constructor() 
+  {
+    this.temperature = "30°C";
+    this.humidity = "60%";
+    this.commentary = "risque d'averces";
+    this.location = "localisation";
+    this.visualisation = "rainy";
+  }
 
 }
