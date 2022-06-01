daf157b93e289b5ecd0c0961b26956af17fe2b4e
madesbouys <maxence.desbouys@etu.uca.fr>
2022-06-01 19:26:29 +0200

    CityCurrentWeather

--- Izanami/src/services/weather-service.service.ts
+++ Izanami/src/services/weather-service.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherServiceService {
 
-  constructor() { }
+  constructor(
+    private _http : HttpClient, 
+  ) {
+
+   }
+
+   public getWeatherFromCity(city : string){
+
+    let params = new HttpParams().set('q', city);
+
+     return this._http.get<any>('https://api.openweathermap.org/data/2.5/weather',{params});
+   }
+
+
+
+
 }
