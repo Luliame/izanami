11d13d846695146f1dabde7e1f1d3b4a844f2a78
Luliame <guillaume.valentin@etu.uca.fr>
2022-06-22 21:00:58 +0200

    IWeather updated

--- Izanami/src/app/weather-presenter/weather-presenter.component.ts
+++ Izanami/src/app/weather-presenter/weather-presenter.component.ts
@@ -31,57 +31,6 @@ export class WeatherPresenterComponent implements OnInit {
 
   public cities = []; 
 
-  public visuList = [
-    "sunny",
-    "partly-sunny",
-    "cloudy",
-    "rainy",
-    "thunderstorm",
-  
-    "cloudy-night",
-    "moon"
-  ];
-
-  coldVal = 0;
-  hotVal = 25;
-
-  dryVal = 25;
-  wetVal = 75;
-
-  coldEmoji = "🥶 ";
-  defaultTempEmoji = "🌡 ";
-  hotEmoji = "🥵 ";
-  temperatureEmoji = this.defaultTempEmoji;
-
-  dryEmoji = "🏜 ";
-  defaultHumEmoji = "💧 ";
-  wetEmoji = "🌊 ";
-  humidityEmoji = this.defaultHumEmoji;
-
-  resolveTemperatureEmoji() {
-    if (this.weather.temperature <= this.coldVal){
-      this.temperatureEmoji = this.coldEmoji;
-    }
-    else if(this.weather.temperature >= this.hotVal){
-      this.temperatureEmoji = this.hotEmoji;
-    }
-    else{
-      this.temperatureEmoji = this.defaultTempEmoji;
-    }
-  }
-
-  resolveHumidityEmoji() {
-    if (this.weather.humidity <= this.dryVal){
-      this.humidityEmoji = this.dryEmoji;
-    }
-    else if(this.weather.humidity >= this.wetVal){
-      this.humidityEmoji = this.wetEmoji;
-    }
-    else{
-      this.humidityEmoji = this.defaultHumEmoji;
-    }
-  }
-
   constructor(
     private _weatherService : WeatherServiceService
   ) 
@@ -89,39 +38,7 @@ export class WeatherPresenterComponent implements OnInit {
 
   ngOnInit() {
     printCurrentPosition();
-
-    this.weather = {
-      temperature : 0,
-      humidity : 0,
-      commentary : "",
-      location : "",
-      visualisation : ""
-    };
-    
-    this.resolveHumidityEmoji();
-    this.resolveTemperatureEmoji();
-
-    // this.loadWeather("Clermont-Ferrand");
   }
-
-  
-  // // Méthode de chargement de la météo selon la ville fournie en paramètre 
-  // loadWeather(city : string){
-  //   // c'est pété !
-  //   this._weatherService.getWeatherFromCity(city).toPromise().then( weather => {
-  //     var i : IWeather = {
-  //       temperature : weather.main.temp,
-  //       humidity : weather.main.humidity,
-  //       commentary : weather.weather[0].description,
-  //       location : weather.name,
-  //       visualisation : weather.weather[0].description
-  //     };
-  //     this.weather = i;
-
-  //     this.resolveHumidityEmoji();
-  //     this.resolveTemperatureEmoji();
-  //   }).catch(error => { console.log("loading error",error)});
-  // }
 
   // méthode autocomplétion de Ville 
   loadCity(search : string){
